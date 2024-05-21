8a710fe9cd506d7704d398af56c32e1dbdec096e
Rolly Maulana Awangga <awangga@gmail.com>
2024-05-21 13:03:17 +0700

    alihin ke auth

--- index.js
+++ index.js
@@ -4,5 +4,6 @@ import { wauthparam } from "https://cdn.jsdelivr.net/gh/whatsauth/js@0.2.1/confi
 wauthparam.auth_ws="d3NzOi8vYXBpLndhLm15LmlkL3dzL3doYXRzYXV0aC9wdWJsaWM=";
 //wauthparam.keyword="aHR0cHM6Ly93YS5tZS82Mjg5NTgwMDAwNjAwMD90ZXh0PXdoNHQ1YXV0aDA=";
 wauthparam.keyword="aHR0cHM6Ly93YS5tZS82Mjg5NTYwMTA2MDAwMD90ZXh0PXdoNHQ1YXV0aDA=";
+wauthparam.redirect ="/auth"
 deleteCookie(wauthparam.tokencookiename);
 qrController(wauthparam);
