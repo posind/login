0d52f262ec6ce0c0737b62989a95e7174d3569bc
Rolly Maulana Awangga <awangga@gmail.com>
2024-08-08 10:38:22 +0700

    ganti nomor iteung

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import { wauthparam } from "https://cdn.jsdelivr.net/gh/whatsauth/js@0.2.1/confi
 
 wauthparam.auth_ws="d3NzOi8vYXBpLndhLm15LmlkL3dzL3doYXRzYXV0aC9wdWJsaWM=";
 //wauthparam.keyword="aHR0cHM6Ly93YS5tZS82Mjg5NTgwMDAwNjAwMD90ZXh0PXdoNHQ1YXV0aDA=";
-wauthparam.keyword="aHR0cHM6Ly93YS5tZS82Mjg5NTM0MDAwMzAwMD90ZXh0PXdoNHQ1YXV0aDA=";
+wauthparam.keyword="aHR0cHM6Ly93YS5tZS82Mjg5NTgwMDAwNjAwMD90ZXh0PXdoNHQ1YXV0aDA=";
 wauthparam.tokencookiehourslifetime=18;
 wauthparam.redirect ="/auth"
 deleteCookie(wauthparam.tokencookiename);
